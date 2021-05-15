0d97aa5f8e21fa122ec15757c20a2d1af41b08e5
Jessica Dean <thething123789@yahoo.com>
2021-05-14 20:34:11 -0500

    layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,4 +11,10 @@ function createFeatures(earthquakeData) {
         id: "mapbox.light",
         accessToken: API_KEY
       });
-
+      var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 15,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+      });
+    
