681aad4ffeac9715e53d9b8e69117bb00e4e140c
Jessica Dean <thething123789@yahoo.com>
2021-05-16 15:38:15 -0500

    darklayermap

--- static/js/logic.js
+++ static/js/logic.js
@@ -59,7 +59,7 @@ function createMap(earthquakes) {
     accessToken: Api_key
   });
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var darklayermap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
@@ -69,7 +69,7 @@ function createMap(earthquakes) {
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": stmap,
-    "Dark Map": darkmap
+    "Dark Map": darklayermap
   };
 
   // Create overlay object to hold our overlay layer
